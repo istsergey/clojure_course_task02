99f06e74766e758fe36f39be62f8b51927814e00
istsergey <istsergey@gmail.com>
2013-03-27 21:32:34 +0300

    first solution

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -2,14 +2,20 @@
   (:gen-class))
 
 
+(defn getName[file]  
+        (.getName file)
+        )
+
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (map getName (filter #(.isFile %) (filter #(re-find (re-pattern file-name)
+          (getName %)) (file-seq (clojure.java.io/file path)))))
+  )
+
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
 
-(defn -main [file-name path]
+(defn -main [file-name path] 
   (if (or (nil? file-name)
           (nil? path))
     (usage)
